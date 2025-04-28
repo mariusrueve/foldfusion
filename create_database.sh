1d144877f43fb073f2d11f367a675496c5ecee31
mrueve <marius.rueve@studium.uni-hamburg.de>
2025-04-28 10:40:51 +0200

    Update database generation path in create_database.sh

--- create_database.sh
+++ create_database.sh
@@ -1,1 +1,1 @@
-/home/stud2022/mrueve/local/opt/siena_1.3.0/generate_siena_database -b ./siena_db.db -d /home/stud2022/mrueve/local/pde10a-pdb-download/compressed/ -f 0
+/home/stud2022/mrueve/local/opt/siena_1.3.0/generate_siena_database -b ./siena_db.db -d /local/mrueve/data/PDBFiles/ -f 0
